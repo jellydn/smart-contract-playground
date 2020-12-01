c1f68cd4c498ad8ca5c9807828c738b6afa1e17a
Huynh Duc Dung <dung@aircarbon.co>
2020-12-01 14:55:33 +0800

    feat(cli): suport parse function name from argument

---
+++ deps.ts
@@ -0,0 +1,5 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@5.0.20/dist/ethers.esm.js";
+import yargs from "https://deno.land/x/yargs/deno.ts";
+
+export { config, ethers, yargs };
--- index.ts
+++ index.ts
@@ -1,28 +1,45 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-import ky from "https://deno.land/x/ky@v0.23.0/index.js";
-import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@5.0.20/dist/ethers.esm.js";
+import { config, ethers, yargs } from "./deps.ts";
 
 (async () => {
   const { API_URL } = config();
   if (!API_URL) throw new Error("Missing API_URL on .env file.");
-  // @ts-expect-error Property 'get' does not exist on type
-  const { addr, network_id: networkId, abi } = await ky.get(API_URL).json();
+  const { addr, network_id: networkId, abi } = await fetch(
+    API_URL
+  ).then((resp) => resp.json());
 
   const networkMapping: Record<number, string> = {
     3: "https://ropsten.infura.io/v3/f48d2390e5f6481286e37db0cf443116",
-    56: "https://ac-prod1.aircarbon.co:9545",
+    56: "https://bsc-dataseed.binance.org",
     97: "https://data-seed-prebsc-1-s1.binance.org:8545",
   };
   console.warn({ addr, networkId, abi });
   const url = networkMapping[networkId];
 
   if (!url) throw new Error(`Not supported yet for ${networkId}`);
-  const provider = new ethers.providers.JsonRpcProvider(url);
+  const provider = new ethers.providers.JsonRpcProvider(url, networkId);
 
   const contract = new ethers.Contract(addr, JSON.parse(abi), provider);
-  console.warn(contract);
-  // @ts-expect-error Property 'getSecTokenTypes' does not exist on type 'Contract'.
-  const value = await contract.getSecTokenTypes();
+  // deno-lint-ignore no-explicit-any
+  (yargs(Deno.args) as any)
+    .command(
+      "viewer <fnName>",
+      "call viewer function from ABI",
+      (yargs: {
+        positional: (arg0: string, arg1: { describe: string }) => any;
+      }) => {
+        return yargs.positional("fnName", {
+          describe: "Viewer function name",
+        });
+      },
+      // deno-lint-ignore no-explicit-any
+      async (argv: any) => {
+        console.info(argv);
+        // deno-lint-ignore no-explicit-any
+        const value = await (contract as any)[argv.fnName]();
 
-  console.warn(value);
+        console.warn(value);
+      }
+    )
+    .strictCommands()
+    .demandCommand(1).argv;
 })();
